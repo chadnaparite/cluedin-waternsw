78ae82c6eab68a254b2d21e1c07a990c0a9efa41
Richard Naparite <chad.naparite@gmail.com>
2022-01-13 11:36:00 +0800

    Update BaseRelatedEntitiesProvider

--- src/WaterNSW.Provider/SuggestedSearches/BaseRelatedEntitiesProvider.cs
+++ src/WaterNSW.Provider/SuggestedSearches/BaseRelatedEntitiesProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using CluedIn.Core;
 using CluedIn.Core.Data;
+using CluedIn.DataStore.Document.Models;
 using CluedIn.RelatedEntities;
 using Microsoft.Extensions.Logging;
 
@@ -11,40 +12,47 @@ namespace CluedIn.Provider.WaterNSW.SuggestedSearches
     public abstract class BaseRelatedEntitiesProvider : IRelatedEntitiesProvider
     {
         public readonly EntityType EntityType;
-        public BaseRelatedEntitiesProvider(EntityType entityType)
+        public readonly string OriginName;
+
+        public BaseRelatedEntitiesProvider(EntityType entityType, string originName = null)
         {
             EntityType = entityType;
+            OriginName = originName;
         }
 
-        public abstract IEnumerable<DataStore.Document.Models.SuggestedSearch> SuggestedSearches(Guid id);
-        public List<DataStore.Document.Models.SuggestedSearch> searches = new List<DataStore.Document.Models.SuggestedSearch>();
+        public abstract IEnumerable<SuggestedSearch> SuggestedSearches(Guid id);
 
-        public IEnumerable<DataStore.Document.Models.SuggestedSearch> GetRelatedEntitiesSearches(ExecutionContext context, Entity entity)
+        public IEnumerable<SuggestedSearch> GetRelatedEntitiesSearches(ExecutionContext context, Entity entity)
         {
-            if (entity.Type == EntityType)
+            var Log = context.Log;
+
+            if (entity.Type != EntityType || (entity.OriginEntityCode.Origin != OriginName && OriginName != null))
             {
-                var Log = context.Log;
-                Log.LogInformation($"[Related Entities] GetRelatedEntitiesSearches({context}, {entity})");
-
-                foreach (var suggestedSearch in SuggestedSearches(entity.Id))
-                {
-                    try
-                    {
-                        if (RelatedEntitiesUtility.CypherFluentQueriesCount(suggestedSearch.SearchQuery, suggestedSearch.Tokens, context) > 0)
-                            searches.Add(suggestedSearch);
-                        else
-                            Log.LogInformation($"[Related Entities] No result: {suggestedSearch.SearchQuery}");
-                    }
-                    catch (Exception ex)
-                    {
-                        Log.LogCritical(ex, "[Related Entities] Error in executing Suggested Search: " + suggestedSearch.DisplayName);
-                    }
-                }
-
-                return searches;
+                Log.LogInformation($"[Related Entities] {EntityType}{", " + OriginName}:  - nothing to suggest");
+                return new SuggestedSearch[0];
             }
 
-            return new DataStore.Document.Models.SuggestedSearch[0];
+            var searches = new List<SuggestedSearch>();
+
+            foreach (var suggestedSearch in SuggestedSearches(entity.Id))
+            {
+                try
+                {
+                    if (RelatedEntitiesUtility.CypherFluentQueriesCount(suggestedSearch.SearchQuery, suggestedSearch.Tokens, context) > 0)
+                    {
+                        Log.LogInformation($"[Related Entities] {EntityType}{", " + OriginName}: CypherFluentQueries matches - {suggestedSearch.DisplayName}");
+                        searches.Add(suggestedSearch);
+                    }
+                    else
+                        Log.LogInformation($"[Related Entities] {EntityType}{", " + OriginName}: CypherFluentQueries does not match - {suggestedSearch.DisplayName}");
+                }
+                catch (Exception ex)
+                {
+                    Log.LogCritical(ex, $"[Related Entities] {EntityType}{", " + OriginName}: Error in executing Suggested Search. Query: '{suggestedSearch.SearchQuery}' Token: '{suggestedSearch.Tokens}'");
+                }
+            }
+
+            return searches;
         }
     }
 }
